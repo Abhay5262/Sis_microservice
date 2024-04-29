b1ca6a0488752fefe1f43b619c11659f0e16d623
Abhay5262 <abhayjoshi.ce20@sltiet.com>
2024-04-29 17:23:06 +0530

    eureka

---
+++ src/test/java/com/Sus_Eureka/SisEurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Sus_Eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SisEurekaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
